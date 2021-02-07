71d2e6019f8bd4ac27fdf7faa396556074dd4eae
Rajan Patel <rajan.n.patel.97@gmail.com>
2021-02-07 15:07:10 +0000

    added hooks version

---
+++ build/precache-manifest.30e5b2676cbdb58f86c15ad1f13479eb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fe99383307299a94169a1c3cb14a05b",
+    "url": "/RoboReactWithHooks/index.html"
+  },
+  {
+    "revision": "220a0570ac4fa4fa9e31",
+    "url": "/RoboReactWithHooks/static/css/2.038644eb.chunk.css"
+  },
+  {
+    "revision": "ac84f4845a71ef6c73c8",
+    "url": "/RoboReactWithHooks/static/css/main.9c396884.chunk.css"
+  },
+  {
+    "revision": "220a0570ac4fa4fa9e31",
+    "url": "/RoboReactWithHooks/static/js/2.765c0106.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RoboReactWithHooks/static/js/2.765c0106.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac84f4845a71ef6c73c8",
+    "url": "/RoboReactWithHooks/static/js/main.7be7a108.chunk.js"
+  },
+  {
+    "revision": "e0d6ce910a8ca6f859a7",
+    "url": "/RoboReactWithHooks/static/js/runtime-main.629b29dc.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RoboReactWithHooks/static/media/SEGA.12a3db8b.woff"
+  }
+]);
